778f7bf3948199e7bd19e868d6ebc4a470cf705e
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-08-22 20:58:09 +0300

    Создан http-сервер

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+// Функция обработчик
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Print("Hello")
+}
+
+func main() {
+	http.HandleFunc("/hello", hello) // Обработка запроса к конкретному запросу
+	fmt.Println("Server is listening on port 8081")
+	http.ListenAndServe(":8081", nil) // Слушаем TCP-соединение на заданном порту
 }
